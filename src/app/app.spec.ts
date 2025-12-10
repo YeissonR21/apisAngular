72f956ed4d58ca88aa4219725d2daa55501910b9
Yeisson Romero <yeisondr2103@gmail.com>
2025-12-09 22:13:52 -0500

    parte inicial del trabajo final de consumo de apis

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, apisAngular');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, consumoapi_personajesNq');
   });
 });
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,17 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+
+import { NavbarComponent } from './components/navbar/navbar'; 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+
+  imports: [CommonModule, RouterOutlet, NavbarComponent ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('apisAngular');
+  title = 'consumo_apis_angular';
 }
